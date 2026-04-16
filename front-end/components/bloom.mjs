f4a031b6f568ad2a85dba9500d48bf443f097a06
katarzynakaz <l453r3@gmail.com>
2026-04-16 14:45:30 +0100

    Fix hashtag link regex

--- front-end/components/bloom.mjs
+++ front-end/components/bloom.mjs
@@ -37,7 +37,7 @@ const createBloom = (template, bloom) => {
 function _formatHashtags(text) {
   if (!text) return text;
   return text.replace(
-    /\B#[^#]+/g,
+    /\B#[a-zA-Z0-9_]+/g,
     (match) => `<a href="/hashtag/${match.slice(1)}">${match}</a>`
   );
 }
