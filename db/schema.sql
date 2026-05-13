f6078182f5a9e7fe9f11b7cd43d974f833a519d8
katarzynakaz <142831031+katarzynakaz@users.noreply.github.com>
2026-05-13 12:08:04 +0100

    Modify schema to replace original_sender with original_bloom_id

--- db/schema.sql
+++ db/schema.sql
@@ -18,8 +18,7 @@ CREATE TABLE blooms (
     sender_id INT NOT NULL REFERENCES users(id),
     content TEXT NOT NULL,
     send_timestamp TIMESTAMP NOT NULL,
-    riginal_sender VARCHAR, 
-    rebloom_count INT DEFAULT 0
+    original_bloom_id BIGINT REFERENCES blooms(id)
 );
 
 CREATE TABLE follows (
